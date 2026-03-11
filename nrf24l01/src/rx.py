6aba8339626fa4fb8c4018d604d697cd4e1fc11e
Bekhruz Malikov <bekhruz.malikov@gmail.com>
2026-03-10 22:43:46 -0700

    Updated rx and rx_test files

--- nrf24l01/src/rx.py
+++ nrf24l01/src/rx.py
@@ -1,6 +1,7 @@
 from machine import Pin, SPI
 from time import sleep
 from nrf24l01 import NRF24L01
+import struct 
 
 # pin wiring
 led = Pin(1, Pin.OUT)
@@ -9,13 +10,17 @@ ce = Pin(5, Pin.OUT, value = 0)
 csn = Pin(6, Pin.OUT, value = 1)
 nrf = NRF24L01(spi, csn, ce, payload_size=8)
 
+# set receiver address for transmitter and start listening for any signals 
 nrf.open_rx_pipe(1, b'\xe1\xf0\xf0\xf0\xf0')
 nrf.start_listening()
 while True:
     led.value(0)
     if nrf.any():
         while nrf.any():
-            data = nrf.recv().decode('utf-8', 'ignore').strip()
-        print(data)
+            data = nrf.recv()
+            #.decode('utf-8', 'ignore').strip() # throws unicode error
+        # once data is received, read it 
+        x, y, z = struct.unpack("HHH", data[:6]) # unpacks data into respective values 
+        print(f"x={x},y={y}, z={z}")
         led.value(1)
         sleep(0.01)
